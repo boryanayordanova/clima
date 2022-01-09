4a84a48a0a14144ae88c460632861bdde5c13cea
boryana <boryana.yourdanova@gmail.com>
2022-01-09 19:29:19 +0200

    cm

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+
+        <h1>Контакти</h1>
+
+        
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2908.1968964975554!2d27.91262355086862!3d43.20535938918957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x40a453f599baacab%3A0x6b5b263e96d18bbd!2z0YPQuy4g4oCe0JTRgNCw0LPQsNC9INCm0LDQvdC60L7QsuKAnCAyNSwgOTAwMCDQktCw0YDQvdCwINCm0LXQvdGC0YrRgCwg0JLQsNGA0L3QsA!3m2!1d43.205355499999996!2d27.9148176!4m5!1s0x40a453f599baacab%3A0x6b5b263e96d18bbd!2z0YPQu9C40YbQsCDigJ7QlNGA0LDQs9Cw0L0g0KbQsNC90LrQvtCy4oCcIDI1LCDQktCw0YDQvdCw!3m2!1d43.205355499999996!2d27.9148176!5e0!3m2!1sbg!2sbg!4v1640306071296!5m2!1sbg!2sbg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+            <h2>Aдрес</h2> 
+        <p>ул. „Драган Цанков“ 25, 9000 Варна Център, Варна</p>
+
+        <h2>Работно Време</h2>
+
+        <ul>
+            <li>Понеделник: <strong>9:00 ч. – 18:00 ч.</strong></li>
+            <li>Вторник: <strong>9:00 ч. – 18:00 ч.</strong></li>
+            <li>Сряда: <strong>9:00 ч. – 18:00 ч.</strong></li>
+            <li>Четвъртък: <strong>9:00 ч. – 18:00 ч.</strong></li>
+            <li>Петък: <strong>9:00 ч. – 18:00 ч.</strong></li>
+            <li>Събота: <strong>9:00 ч. – 14:00 ч.</strong></li>
+            <li>Неделя: <strong>Затворено</strong></li>
+        </ul>
+
+        <h2>Email Адрес</h2>
+        <p><a href="mailto:klimastil@gmail.com">klimastil@gmail.com</a></p>
+
+        <h2>Телефонен Номер</h2>
+        <p> <a href="tel:0888888888">0888888888</a></p>
+
+
+        <h2>Последвайте ни</h2>
+        <a href="https://www.facebook.com/%D0%9A%D0%BB%D0%B8%D0%BC%D0%B0-%D0%A1%D1%82%D0%B8%D0%BB-%D0%92%D0%B0%D1%80%D0%BD%D0%B0-%D0%9E%D0%9E%D0%94-1806246189681207/?ref=page_internal" target="_blank">Facebook</a>
+
+        
+        
+
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Клима Стил - Варна') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    
+    
+   
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> -->
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Clima Style Varna') }}
+                </a>
+                <i class="fas fa-wind"></i>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                      
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <!-- @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Вход') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
+                                </li>
+                            @endif -->
+                            <li><a class="nav-link" href="{{ route('about') }}">Начало</a></li>
+                            <li><a class="nav-link" href="{{ route('about') }}">Продукти</a></li>
+                            <li><a class="nav-link" href="{{ route('about') }}">Марки</a></li>
+                            <li><a class="nav-link" href="{{ route('about') }}">Промоции</a></li>
+                            <li><a class="nav-link" href="{{ route('about') }}">Услуги</a></li>
+
+                            <li><a class="nav-link" href="{{ route('about') }}">За Нас</a></li>
+                            <li><a class="nav-link" href="{{ route('contacts') }}">Контакти</a></li>
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
